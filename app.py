37003217cf314a5d3dbd7f1cdd1765c22b6b7c3e
Darunesh.R <daru22018.it@rmkec.ac.in>
2024-03-04 23:41:06 +0530

    have added box container to the html created a dict() string in python created bookitems and not even finished halfway

--- app.py
+++ app.py
@@ -2,10 +2,59 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+BOOKS=[
+    {
+        'id':123,
+        'Title':'Harry Potter',
+        'Author':'J.K Rowling',
+        'Volume':7,
+        'Price':500,
+        'Rating':4.7,
+        'Year': 1998,
+        
+        
+    },
+    
+    {
+        'id':125,
+        'Title':'Percy Jackson & the Olympians',
+        'Author':'Rick Riordan',
+        'Volume':1,
+        'Price':500,
+        'Rating':4.7,
+        'Year': 2005,
+        
+        
+    },
+    
+    {
+        'id':122,
+        'Title':'Goosebumps',
+        'Author':'R. L. Stine',
+        'Volume':6,
+        'Price':500,
+        'Rating':4.2,
+        'Year': 1998,
+        
+        
+    },
+    
+    {
+        'id':120,
+        'Title':'The Twin Serpents',
+        'Author':'Ronald Scott Thorn',
+        'Volume':6,
+        'Price':500,
+        'Rating':4.2,
+        'Year': 1965,
+        
+        
+    },
+]
 
 @app.route('/')
 def hell_world():
-    return render_template('home.html')
+    return render_template('home.html', books=BOOKS)
 
 if __name__=="__main__":
     app.run(debug=True)
